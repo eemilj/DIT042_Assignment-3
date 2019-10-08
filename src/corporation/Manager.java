3fe3832ccc1b681f3558fcfec2688e5b5777f289
ironmansmacbookpro2 <gushjeros@student.gu.se>
2019-10-08 17:00:43 +0200

    asdasd

--- src/corporation/Manager.java
+++ src/corporation/Manager.java
@@ -1,5 +1,12 @@
 package corporation;
 
-public class Manager {
+public class Manager extends Employee {
+
+    String degree;
+
+    public Manager(String degree, String name, String ID, double grossSalary) {
+        super(name, ID, grossSalary);
+        this.degree = degree;
+    }
 
 }
