55841efd9e8fd889d835603d0aa4e685cf74f736
eemilj <jeskanen.eemil@gmail.com>
2019-10-10 16:02:30 +0200

    Dirpay method

--- src/corporation/Director.java
+++ src/corporation/Director.java
@@ -5,13 +5,18 @@ public class Director extends Manager {
 
 
     private String department;
-    private double dirPay = getGrossSalary()  + ReusaxCorp.getDirectorBenefit();
+    private double dirPay;
 
 
     public Director(String name, String id, double grossSalary, String degree, String department) {
         super(name, id, grossSalary,degree);
         this.department = department;
+        dirPay = getDirPay();
 
+    }
+    public double getDirPay(){
+        dirPay =  getGrossSalary() + getManBenefit() + ReusaxCorp.getDirectorBenefit();
+        return dirPay;
     }
     public void setDepartment(String endUserInput){
         this.department = endUserInput;
